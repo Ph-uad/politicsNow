b38b16b9db99860eec3c28b1c18ec5c9f39e9ce3
Ph-uad <temidayo26j@gmail.com>
2023-04-26 11:55:26 +0100

    Navigation update

--- components/UI/MainNavigation.js
+++ components/UI/MainNavigation.js
@@ -1,43 +1,52 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '@/styles/Nav.module.css';
+import { useRouter } from 'next/router';
 
+
+const dateObj = new Date();
+const DAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const date = `${DAY[dateObj.getDay()] + ', ' + MONTH[dateObj.getMonth()] + '. ' + dateObj.getUTCDate() + ' ' + dateObj.getFullYear()}`
 
 
 const MainNavigation = () => {
 
-  const date = new Date();
-  const DAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-  // const changeHandler = () => {
-  //   document.body.classList.toggle('dark')
-  // }
+  const router = useRouter();
 
   return (
-    <nav className=''>
+    <nav className={ styles.main_nav }>
+
       <div className={ ` ${styles["nav-container"]} flex` }>
 
-        <p className='date'>{ DAY[date.getDay()] }</p>
-
-
+        <p className='title--small'>{ date }</p>
         <Link href='/' className={ `${styles.link} link` } as='/'>
+
           <Image className={ styles.logo } src="/logo.png" priority fill alt="logo" sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw" />
+
         </Link>
-
-
-        <p className='date'>{ date.getUTCDate() } { MONTH[date.getMonth()] }. { date.getFullYear() }</p>
-
+        <span></span>
       </div>
 
       <div className={ `${styles["nav-container"]}  flex` }>
-        <ul className={ `${styles.list} flex` }>
-          <li className={ styles.list_link }> <Link className='link' href='/' > Home </Link></li>
-          <li className={ styles.list_link }> <Link className='link' href='policies'>  Policies </Link></li>
-          <li className={ styles.list_link }> <Link className='link' href='feature'>  Feauture </Link></li>
-          <li className={ styles.list_link }> <Link className='link' href='opinion'>  Opinion </Link></li>
-          <li className={ styles.list_link }> <Link className='link' href=''>  Backpage </Link></li>
-        </ul>
+
+        <div className={styles['nav-menu']}>
+
+          <input type="checkbox" className={ styles.checkbox } name="nav-responsive" id="nav-responsive" />
+          <label className={ styles.checkbox_label } htmlFor="nav-responsive"> <span className={ styles.checkbox_span }></span> </label>
+
+          <ul className={ styles.list }>
+
+            <li className={ styles.list_link }> <Link className={ `link nav-link ${router.pathname === '/' ? "active" : ''}` } href='/' > Home </Link></li>
+            <li className={ styles.list_link }> <Link className={ `link nav-link ${router.pathname === '/policies' ? "active" : ''}` } href='policies'>/  Policies </Link></li>
+            <li className={ styles.list_link }> <Link className={ `link nav-link ${router.pathname === '/features' ? "active" : ''}` } href='feature'>/  Feauture </Link></li>
+            <li className={ styles.list_link }> <Link className={ `link nav-link ${router.pathname === '/opinion' ? "active" : ''}` } href='opinion'>/  Opinion </Link></li>
+            <li className={ styles.list_link }> <Link className={ `link nav-link ${router.pathname === '/backpage' ? "active" : ''}` } href=''> / Backpage </Link></li>
+
+          </ul>
+
+        </div>
+
         <form className="form">
           <label htmlFor="search">
             <input autoComplete="off" placeholder="Search" id="search" type="text" />
